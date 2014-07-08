4be91049bfc16fbe3fa7415a7a47bf4f49fc3c8a
w0ts0n <w0ts0n@gmail.com>
2014-07-08 10:21:57 -0400

    addded core files

---
+++ script.js
@@ -0,0 +1,397 @@
+/* begin Page */
+
+
+
+var artEventHelper = {
+  'bind' : function (obj, evt, fn) {
+    if (obj.addEventListener)
+      obj.addEventListener(evt, fn, false);
+    else if (obj.attachEvent)
+      obj.attachEvent('on' + evt, fn);
+    else
+      obj['on' + evt] = fn;
+  }
+};
+
+var artLoadEvent = (function() {
+  var userAgent = navigator.userAgent.toLowerCase();
+  var browser = {
+    version: (userAgent.match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/) || [])[1],
+    safari: /webkit/.test(userAgent),
+    opera: /opera/.test(userAgent),
+    msie: /msie/.test(userAgent) && !/opera/.test(userAgent),
+    mozilla: /mozilla/.test(userAgent) && !/(compatible|webkit)/.test(userAgent)
+  };
+
+  var list = [];
+
+  var done = false;
+  var ready = function () {
+    if (done) return;
+    done = true;
+    for (var i = 0; i < list.length; i++)
+      list[i]();
+  };
+  
+  if (document.addEventListener && !browser.opera)
+    document.addEventListener('DOMContentLoaded', ready, false);
+  
+  if (browser.msie && window == top) {
+    (function () {
+      try {
+        document.documentElement.doScroll('left');
+      } catch (e) {
+        setTimeout(arguments.callee, 10);
+        return;
+      }
+      ready();
+    })();
+  }
+  
+  if (browser.opera) {
+    document.addEventListener('DOMContentLoaded', function () {
+      for (var i = 0; i < document.styleSheets.length; i++) {
+        if (document.styleSheets[i].disabled) {
+          setTimeout(arguments.callee, 10);
+          return;
+        }
+      }
+      ready();
+    }, false);
+  }
+  
+  if (browser.safari) {
+    var numStyles;
+    (function(){
+      if (document.readyState != 'loaded' && document.readyState != 'complete') {
+        setTimeout(arguments.callee, 10);
+        return;
+      }
+      if ('undefined' == typeof numStyles) {
+        numStyles = document.getElementsByTagName('style').length;
+        var links = document.getElementsByTagName('link');
+        for (var i = 0; i < links.length; i++) {
+          numStyles += (links[i].getAttribute('rel') == 'stylesheet') ? 1 : 0;
+        }
+        if (document.styleSheets.length != numStyles) {
+          setTimeout(arguments.callee, 0);
+          return;
+        }
+      }
+      ready();
+    })();
+  }
+
+  artEventHelper.bind(window, 'load', ready);
+
+  return ({
+    add: function(f) {
+      list.push(f);
+    }
+  })
+})();
+
+(function () {
+  // fix ie blinking
+  var m = document.uniqueID && document.compatMode && !window.XMLHttpRequest && document.execCommand;
+  try{ if(!!m) { m("BackgroundImageCache", false, true); } }
+  catch(oh){};
+})();
+
+function xGetElementsByClassName(clsName, parentEle, tagName) {
+  var elements = null;
+  var found = [];
+  var slash = String.fromCharCode(92);
+  var re = new RegExp(slash + "b" + clsName + slash + "b");
+  if (!parentEle) parentEle = document;
+  if (!tagName) tagName = '*';
+  elements = parentEle.getElementsByTagName(tagName);
+  if (elements) {
+    for (var i = 0; i < elements.length; ++i) {
+      if (elements[i].className.search(re) != -1) {
+        found[found.length] = elements[i];
+      }
+    }
+  }
+  return found;
+}
+
+var styleUrlCached = null;
+function GetStyleUrl() {
+  if (null == styleUrlCached) {
+    var ns;
+    styleUrlCached = '';
+    ns = document.getElementsByTagName('link');
+    for (var i = 0; i < ns.length; i++) {
+      var l = ns[i];
+      if (l.href && /style\.css(\?.*)?$/.test(l.href)){
+        return styleUrlCached = l.href.replace(/style\.css(\?.*)?$/,'');
+      }
+    }
+    
+    ns = document.getElementsByTagName('style');
+    for (var i = 0; i < ns.length; i++) {
+      var matches = new RegExp('import\\s+"([^"]+\\/)style\\.css"').exec(ns[i].innerHTML);
+      if (null != matches && matches.length > 0)
+        return styleUrlCached = matches[1];
+    }
+  }
+  return styleUrlCached;
+}
+
+
+function artButtonSetupJsHover(btn)
+{
+    artEventHelper.bind(btn, 'mouseover', function() {
+        var spans = btn.getElementsByTagName("span");
+        if (spans.length > 0)
+            spans[0].className += " hover";
+    });
+    artEventHelper.bind(btn, 'mouseout', function() {
+        var spans = btn.getElementsByTagName("span");
+        if (spans.length > 0)
+            spans[0].className = spans[0].className.replace(/hover/, "").replace(/active/, "");
+    });
+    artEventHelper.bind(btn, 'mousedown', function() {
+        var spans = btn.getElementsByTagName("span");
+        if (spans.length > 0)
+            spans[0].className += " active";
+    });
+    artEventHelper.bind(btn, 'mouseup', function() {
+        var spans = btn.getElementsByTagName("span");
+        if (spans.length > 0)
+            spans[0].className = spans[0].className.replace(/active/, "");
+    });
+}
+
+function artButtonsSetupJsHover() {
+  var elements = xGetElementsByClassName("btn", document, "span");
+  for (var i = 0; i < elements.length; i++) {
+    if (!elements[i].tagName) continue;
+    artButtonSetupJsHover(elements[i].parentNode);
+  }
+}
+artLoadEvent.add(artButtonsSetupJsHover);
+
+/* end Page */
+
+/* begin Menu */
+function Insert_Separators()
+{
+  var menus = xGetElementsByClassName("artmenu", document);
+  for (var i = 0; i < menus.length; i++) {
+    var menu = menus[i];
+    var childs = menu.childNodes;
+    var listItems = [];
+    for (var j = 0; j < childs.length; j++){
+      var el = childs[j];
+      if (String(el.tagName).toLowerCase() == "li")listItems.push(el);
+    }
+    for (var j = 0; j < listItems.length - 1; j++){
+      var span = document.createElement('span');
+      span.className = 'separator';
+      var li = document.createElement('li');
+      li.appendChild(span);
+      listItems[j].parentNode.insertBefore(li, listItems[j].nextSibling);
+    }
+  }
+}
+artLoadEvent.add(Insert_Separators);
+
+function Menu_IE6Setup() {
+  var isIE6 = navigator.userAgent.toLowerCase().indexOf("msie") != -1 
+    && navigator.userAgent.toLowerCase().indexOf("msie 7") == -1;
+  if (!isIE6) return;
+  var aTmp2, i, j, oLI, aUL, aA;
+  var aTmp = xGetElementsByClassName("artmenu", document, "ul");
+  for (i=0;i<aTmp.length;i++) {
+    aTmp2 = aTmp[i].getElementsByTagName("li");
+    for (j=0;j<aTmp2.length;j++) {
+      oLI = aTmp2[j];
+      aUL = oLI.getElementsByTagName("ul");
+      if (aUL && aUL.length) {
+        oLI.UL = aUL[0];
+        aA = oLI.getElementsByTagName("a");
+        if (aA && aA.length)
+        	oLI.A = aA[0];
+         oLI.onmouseenter = function() {
+         	this.className += " artmenuhover";
+         	this.UL.className += " artmenuhoverUL";
+         	if (this.A) this.A.className += " artmenuhoverA";
+         };
+        oLI.onmouseleave = function() {
+          this.className = this.className.replace(/artmenuhover/,"");
+          this.UL.className = this.UL.className.replace(/artmenuhoverUL/,"");
+          if (this.A) this.A.className = this.A.className.replace(/artmenuhoverA/, "");
+        };
+      }
+    }
+  }
+}
+artLoadEvent.add(Menu_IE6Setup);
+/* end Menu */
+
+ var level = 0;
+
+ var commands = new Array("echo", "cd", "color");
+
+ 
+ function levelup(){
+    var challenge = new Array("ssh root@downloadmoreram.com", "cat /etc/passwd", "rm -rf /");
+    //loaded =0; 
+    //waitTime -= 2;
+    //alert(waitTime);
+    if (level >= (challenge.length - 1)) {
+        alert("Challenge Complete");
+        document.getElementById("command").innerHTML = "You hacked " + Math.pow(2, (level+1)) + " RAMs today";
+    } else if (loaded == 200) {
+        alert("Game Over");
+        document.getElementById("command").innerHTML = "Game Over";
+    } else {
+        level++;
+        loaded -= 5;
+        document.getElementById("command").innerHTML = challenge[level];
+    }
+ }
+
+ function execute(arr, myCmd, args) { 
+  for(var i=0; i<arr.length; i++) { 
+       if (arr[i] == myCmd){ 
+            return run(myCmd, args);
+            break;
+       } else {
+            var newCmd = myCmd + " " + args;
+            if(newCmd.trim() == document.getElementById("command").innerHTML.trim()) {
+                levelup();
+                return "Level Up!";
+                break;
+           } else {
+                return false;
+                break;
+           } 
+        }
+    } 
+ }
+ 
+ function run(obj, args){
+  var str;
+  switch(obj){
+   case "echo":
+    str = args;
+    break;
+   case "color":
+    var allowed = Array("0", "1", "2", "3", "4", "5", "6", "7","9", "A",
+        "B", "C", "D", "E", "F");
+    var bg_color = args.substring(0, 1);
+    var font_color = args.substring(2,1);
+    document.bgColor = "#" + changeColor(bg_color);
+    document.getElementById('bodyID').style.color = "#" + changeColor(font_color);
+    document.getElementById("txtBox").style.color = "#" + changeColor(font_color);
+    break;
+  } 
+  return str;
+ }
+ 
+ function changeColor(aColor){
+  var color = "";
+  switch(aColor){
+   case "0":
+    color = "000000";
+    break;
+   case "1":
+    color = "00008B";
+    break;
+   case "2":
+    color = "008000";
+    break;
+   case "3":
+    color = "00FFFF";
+    break;
+   case "4":
+    color = "FF0000";
+    break;
+   case "5":
+    color = "800080";
+    break;
+   case "6":
+    color = "FFFF00";
+    break;
+   case "7":
+    color = "D3D3D3";
+    break;
+   case "8":
+    color = "808080";
+    break;
+   case "9":
+    color = "0000FF";
+    break;
+   case "a":
+    color = "00FF00";
+    break;
+   case "b":
+    color = "E0FFFF";
+    break;
+   case "c":
+    color = "FF6347";
+    break;
+   case "d":
+    color = "9370D8";
+    break;
+   case "e":
+    color = "F4FA58";
+    break;
+   case "f":
+    color = "FFFFFF";
+    break;
+
+  }
+  return color;  
+ }
+ function cmd(){
+  var args = "";
+  var box = document.getElementById("txtBox");
+  var cmdArr = box.value.split(" ");
+  var myCmd = cmdArr[0];
+  for(var i=1; i<cmdArr.length; i++){
+   args += cmdArr[i] + " ";
+  } 
+  var myEle = document.createElement("span");
+  myEle.innerHTML = "$&nbsp;" + htmlspecialchars(box.value) + "<br />";
+  
+  var runCmd = execute(commands, myCmd, args);
+  if(runCmd == false){
+   myEle.innerHTML += "\""+htmlspecialchars(box.value)+"\" is not a recognized command<br /><br />";
+  }
+  else if(runCmd != null){
+   myEle.innerHTML += htmlspecialchars(runCmd) + "<br /><br />";
+  }
+  document.getElementById("txtDiv").appendChild(myEle);
+  box.value = "";
+ }
+
+ function submitenter(myfield,e){
+  var keycode;
+  if (window.event) keycode = window.event.keyCode;
+  else if (e) keycode = e.which;
+  else return true;
+
+  if (keycode == 13){
+      cmd();
+   return false;
+  }
+  else
+      return true;
+ }
+ 
+ function htmlspecialchars(str) {
+  if (typeof(str) == "string") {
+    str = str.replace(/&/g, "&amp;"); /* must do &amp; first */
+    str = str.replace(/"/g, "&quot;");
+    str = str.replace(/'/g, "&#039;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/>/g, "&gt;");
+ }
+  return str;
+  }
+
+
---
+++ timerbar.js
@@ -0,0 +1,134 @@
+// Timer Bar - Version 1.0
+// Author: Brian Gosselin of http://scriptasylum.com
+// Script featured on http://www.dynamicdrive.com
+
+var loadedcolor='blue' ;       // PROGRESS BAR COLOR
+var unloadedcolor='lightgrey';     // COLOR OF UNLOADED AREA
+var bordercolor='navy';            // COLOR OF THE BORDER
+var barheight=30;                  // HEIGHT OF PROGRESS BAR IN PIXELS
+var barwidth=400;                  // WIDTH OF THE BAR IN PIXELS
+var waitTime=20;                   // NUMBER OF SECONDS FOR PROGRESSBAR
+var clickCount = 0;
+
+// THE FUNCTION BELOW CONTAINS THE ACTION(S) TAKEN ONCE BAR REACHES 100%.
+// IF NO ACTION IS DESIRED, TAKE EVERYTHING OUT FROM BETWEEN THE CURLY BRACES ({})
+// BUT LEAVE THE FUNCTION NAME AND CURLY BRACES IN PLACE.
+// PRESENTLY, IT IS SET TO DO NOTHING, BUT CAN BE CHANGED EASILY.
+// TO CAUSE A REDIRECT TO ANOTHER PAGE, INSERT THE FOLLOWING LINE:
+// window.location="http://redirect_page.html";
+// JUST CHANGE THE ACTUAL URL OF COURSE :)
+
+var action=function()
+{
+    
+    if (loaded == 200) {
+        alert("Download Complete")    
+    } else if(clickCount == 1) {
+        alert("HEY! Stop that!");
+        progressBarInit();
+    } else if (clickCount == 2) {
+        openTerminal();
+        loaded = 0;
+        progressBarInit();
+    } else {
+        progressBarInit();
+    }
+}
+
+function openTerminal(){
+    el = document.getElementById("terminal");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+
+    el2 = document.getElementById("command");
+    el2.style.visibility = (el2.style.visibility == "visible") ? "hidden" : "visible";
+
+    document.getElementById('txtBox').focus();
+}
+
+function incrementCount() {
+    clickCount++;
+    alert();
+}
+
+//*****************************************************//
+//**********  DO NOT EDIT BEYOND THIS POINT  **********//
+//*****************************************************//
+
+var ns4=(document.layers)?true:false;
+var ie4=(document.all)?true:false;
+var blocksize=(barwidth-2)/waitTime/10;
+var loaded=0;
+var PBouter;
+var PBdone;
+var PBbckgnd;
+var Pid=0;
+var txt='';
+if(ns4){
+txt+='<table border=0 cellpadding=0 cellspacing=0><tr><td>hello world';
+txt+='<ilayer name="PBouter" visibility="hide" height="'+barheight+'" width="'+barwidth+'" onmouseup="hidebar()" onclick="clickCount++"><p>hello world</p>';
+txt+='<layer width="'+barwidth+'" height="'+barheight+'" bgcolor="'+bordercolor+'" top="0" left="0"><p>hello world</p></layer>';
+txt+='<layer width="'+(barwidth-2)+'" height="'+(barheight-2)+'" bgcolor="'+unloadedcolor+'" top="1" left="1"><p>hello world</p></layer>';
+txt+='<layer name="PBdone" width="'+(barwidth-2)+'" height="'+(barheight-2)+'" bgcolor="'+loadedcolor+'" top="1" left="1"><p>hello world</p></layer>';
+txt+='</ilayer>';
+txt+='</td></tr></table>';
+}else{
+txt+='<div id="PBouter" onmouseup="hidebar()" onclick="clickCount++" style="position:relative; visibility:hidden; background-color:'+bordercolor+'; width:'+barwidth+'px; height:'+barheight+'px;">';
+txt+='<div style="position:absolute; top:1px; left:1px; width:'+(barwidth-2)+'px; height:'+(barheight-2)+'px; background-color:'+unloadedcolor+'; font-size:1px;"></div>';
+txt+='<div id="PBdone" style="position:absolute; top:1px; left:1px; width:0px; height:'+(barheight-2)+'px; background-color:'+loadedcolor+'; font-size:1px;"></div>';
+txt+='</div>';
+}
+
+document.write(txt);
+
+function incrCount(){
+window.status="Loading...";
+loaded++;
+if(loaded<0)loaded=0;
+if(loaded>=waitTime*10){
+clearInterval(Pid);
+loaded=waitTime*10;
+setTimeout('hidebar()',100);
+}
+resizeEl(PBdone, 0, blocksize*loaded, barheight-2, 0);
+}
+
+function hidebar(){
+clearInterval(Pid);
+window.status='';
+//if(ns4)PBouter.visibility="hide";
+//else PBouter.style.visibility="hidden";
+action();
+}
+
+//THIS FUNCTION BY MIKE HALL OF BRAINJAR.COM
+function findlayer(name,doc){
+var i,layer;
+for(i=0;i<doc.layers.length;i++){
+layer=doc.layers[i];
+if(layer.name==name)return layer;
+if(layer.document.layers.length>0)
+if((layer=findlayer(name,layer.document))!=null)
+return layer;
+}
+return null;
+}
+
+function progressBarInit(){
+PBouter=(ns4)?findlayer('PBouter',document):(ie4)?document.all['PBouter']:document.getElementById('PBouter');
+PBdone=(ns4)?PBouter.document.layers['PBdone']:(ie4)?document.all['PBdone']:document.getElementById('PBdone');
+resizeEl(PBdone,0,0,barheight-2,0);
+if(ns4)PBouter.visibility="show";
+else PBouter.style.visibility="visible";
+Pid=setInterval('incrCount()',95);
+}
+
+function resizeEl(id,t,r,b,l){
+if(ns4){
+id.clip.left=l;
+id.clip.top=t;
+id.clip.right=r;
+id.clip.bottom=b;
+}else id.style.width=r+'px';
+}
+
+window.onload=progressBarInit;
